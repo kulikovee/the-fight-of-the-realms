253ae9e57f8a3b6c11158a8bf37762c423be2992
Eduard Kulikov <kulikov.e.ev@gmail.com>
2022-11-01 23:14:22 +0200

    Update Players GUI

--- Assets/Scripts/UI/AbilityIcon.cs
+++ Assets/Scripts/UI/AbilityIcon.cs
@@ -7,10 +7,13 @@ public class AbilityIcon : MonoBehaviour
     {
         BOMB,
         REVIVE,
+        ATTACK,
+        JUMP,
     };
 
     public UnitController unit;
     public AbilityType abilityType = AbilityType.BOMB;
+    public GameObject outline;
     Image icon;
 
     void Start()
@@ -21,12 +24,21 @@ public class AbilityIcon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var manaRequired = abilityType == AbilityType.BOMB 
-            ? unit.specialAttackManaCost
-            : unit.spellManaCost;
+        if (abilityType == AbilityType.BOMB || abilityType == AbilityType.REVIVE)
+        {
+            var manaRequired = abilityType == AbilityType.BOMB 
+                ? unit.specialAttackManaCost
+                : unit.spellManaCost;
+            var value = unit.GetMana() < manaRequired
+                ? unit.GetMana() / manaRequired
+                : 1;
 
-        icon.fillAmount = unit.GetMana() < manaRequired
-            ? unit.GetMana() / manaRequired
-            : 1;
+            icon.fillAmount = value;
+
+            if (outline != null)
+            {
+                outline.SetActive(value >= 1);
+            }
+        }
     }
 }
