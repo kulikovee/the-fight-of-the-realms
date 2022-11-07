baeb2f14fafb7f678dfb0612e39ca4702d68ae25
Eduard Kulikov <kulikov.e.ev@gmail.com>
2022-11-07 23:57:08 +0200

    Add lobby

--- Assets/Scripts/UI/LogoController.cs
+++ Assets/Scripts/UI/LogoController.cs
@@ -4,38 +4,38 @@ using UnityEngine.SceneManagement;
 public class LogoController : MonoBehaviour
 {
     public string arenaSceneName = "arena";
-    private Animator animator;
+    public AudioSource hitSound1;
+    public AudioSource waveSound1;
+    public AudioSource waveSound2;
+    public AudioSource showTitleSound;
 
-    void Start()
-    {
-        ActionsController.OnStartGame += Hide;
-
-        animator = GetComponent<Animator>();
-        Time.timeScale = 0;
-    }
-
-    void OnDestroy()
-    {
-        ActionsController.OnStartGame -= Hide;
-    }
-
-    public void Hide()
-    {
-        if (gameObject.activeSelf)
-        {
-            animator.Play("Logo Hide");
-        }
-    }
-
-    /** Called from animation: Logo Idle **/
+    /** Called from animation: Show **/
     public void ShowStartupMenu()
     {
         SceneManager.LoadScene(arenaSceneName);
     }
 
-    /** Called from animation: Logo Hide **/
-    public void Hidden()
+    /** Called from animation: Show **/
+    public void PlayHitSound1()
     {
-        gameObject.SetActive(false);
+        hitSound1.Play();
+    }
+
+    /** Called from animation: Show **/
+    public void PlayWaveSound1()
+    {
+        waveSound1.Play();
+    }
+
+    /** Called from animation: Show **/
+    public void PlayWaveSound2()
+    {
+        waveSound2.Play();
+    }
+
+    /** Called from animation: Show **/
+    public void PlayShowTitleSound()
+    {
+        showTitleSound.Play();
     }
 }
