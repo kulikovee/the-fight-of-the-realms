361d6bb0a9d32676a16894701e52db53a35c36d5
Eduard Kulikov <kulikov.e.ev@gmail.com>
2022-11-10 00:38:49 +0200

    Update Logo animation

--- Assets/Scripts/UI/LogoController.cs
+++ Assets/Scripts/UI/LogoController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,18 +6,39 @@ public class LogoController : MonoBehaviour
 {
     public string arenaSceneName = "arena";
     public AudioSource hitSound1;
+    public AudioSource hitSound2;
     public AudioSource waveSound1;
     public AudioSource waveSound2;
     public AudioSource showTitleSound;
 
+    void Start()
+    {
+        Time.timeScale = 0.75f;
+        StartCoroutine(SetTimeScaleAfterDelay(2.95f, 0.01f));
+        StartCoroutine(SetTimeScaleAfterDelay(2.97f, 0.0075f));
+        StartCoroutine(SetTimeScaleAfterDelay(2.99f, 0.0050f));
+    }
+
+    IEnumerator SetTimeScaleAfterDelay(float delay, float timeScale)
+    {
+        yield return new WaitForSeconds(delay);
+        Time.timeScale = timeScale;
+    }
+
     /** Called from animation: Show **/
     public void ShowStartupMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(arenaSceneName);
     }
 
     /** Called from animation: Show **/
     public void PlayHitSound1()
+    {
+        hitSound1.Play();
+    }
+    /** Called from animation: Show **/
+    public void PlayHitSound2()
     {
         hitSound1.Play();
     }
@@ -32,6 +54,7 @@ public class LogoController : MonoBehaviour
     {
         waveSound2.Play();
     }
+
 
     /** Called from animation: Show **/
     public void PlayShowTitleSound()
